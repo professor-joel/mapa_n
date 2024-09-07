6f9473c7c775ec858a30bc322865b1a8c928bfb3
professor-joel <joel.ramos.prof@gmail.com>
2024-09-07 20:17:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mapa_n
+ criação de um mapa navegacional para ensino de algoritmo com a linguagem de programação Javascript
---
+++ script.js
@@ -0,0 +1,2 @@
+// Aqui você pode adicionar alguma interatividade, se necessário
+// Por exemplo, pode ocultar/mostrar seções dinamicamente conforme a navegação
